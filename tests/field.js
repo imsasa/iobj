43f5cd1848029ddecca10c8d792f90b924310ae5
evanmao <evanmao@tencent.com>
2020-05-20 10:00:32 +0800

    refactor: - 代码优化；

--- tests/field.js
+++ tests/field.js
@@ -1,72 +1,112 @@
 var assert = require("assert");
 import Field from "../src/field";
-import Vue   from "vue"
+// import Vue   from "vue"
 // required 默认为false
 
 
 describe("数据验证", function () {
     let F = new Field({
-        name      : "fage",
-        defaultVal: 4,
-        validator : function () {
-            return this.$val > 3;
+        name        : "fage",
+        defaultValue: 4,
+        validator   : function () {
+            return this.value > 3;
         }
     });
-
-    it("使用默认值01", function () {
+    it("验证数据的初始值01）", function () {
         let foo = new F();
-        assert.equal(true, foo.$isValid);
-        assert.equal(false, foo.$isMod);
-
+        assert.equal(undefined, foo.value);
     });
-    it("使用默认值02", function () {
-        let foo  = new F();
-        foo.$val = 4;
-        assert.equal(true, foo.$isValid);
-        assert.equal(false, foo.$isMod);
+    it("验证数据的初始值02）", function () {
+        let F   = new Field({
+            name        : "fage",
+            defaultValue: 4,
+            required    : true,
+            validator   : function () {
+                return this.value > 3;
+            }
+        });
+        let foo = new F();
+        assert.equal(4, foo.value);
     });
-    it("值为undefined", function () {
-        let foo = new F(1, false, true);
-        assert.equal(true, foo.$isValid);
-    });
-    it("使用错误的初始值01", function () {
+    it("验证数据的初始值（初始值给定错误）", function (done) {
         let foo = new F(2);
-        assert.equal(false, foo.$isValid);
-    });
-
-    it("使用错误的初始值02", function () {
-        let foo  = new F(2);
-        foo.$val = 5;
-        assert.equal(true, foo.$isValid);
-    });
-
-    it("数据验证状态", function (done) {
-        let foo  = new F();
-        foo.$val = 2;
-        foo.$validate().then(
+        foo.validate().then(
             (ret) => {
-                assert.equal(false, foo.$isValid);
+                assert.equal(false, foo.isValid);
+                done();
+            }
+        );
+    });
+    it("验证数据的初始值（初始值给定正确）", function (done) {
+        let foo = new F(8);
+        foo.validate().then(
+            (ret) => {
+                assert.equal(true, foo.isValid);
+                done();
+            }
+        );
+    });
+    it("赋值后验证数据(使用错误的初始值)", function (done) {
+        let foo   = new F(3);
+        foo.value = 6;
+        foo.validate().then(
+            (ret) => {
+                assert.equal(true, foo.isValid);
+                done();
+            }
+        );
+    });
+    it("赋值后验证数据(反复修改01)", function (done) {
+        let foo   = new F(2);
+        foo.value = 5;
+        foo.value = 3;
+        foo.validate().then(
+            (ret) => {
+                assert.equal(false, foo.isValid);
+                done();
+            }
+        );
+    });
+    it("赋值后验证数据(反复修改02)", function (done) {
+        let foo   = new F(2);
+        foo.value = 5;
+        foo.value = 7;
+        foo.value = 8;
+        foo.validate().then(
+            (ret) => {
+                assert.equal(true, foo.isValid);
+                done();
+            }
+        );
+    });
+
+    it("赋值后验证数据(反复修改03)", function (done) {
+        let foo   = new F();
+        foo.value = 5;
+        foo.value = 7;
+        foo.value = 7;
+        foo.validate().then(
+            (ret) => {
+                assert.equal(true, foo.isValid);
                 done();
             }
         );
     });
 });
 
-describe("不使用默认值01", function () {
+describe("监听值变化", function () {
     let F   = new Field({
         name      : "fage",
         defaultVal: 4,
-        validator : function () {
-            return this.$val > 3;
+        validator : function (val) {
+            return val > 3;
         }
     });
-    let foo = new F(undefined, true);
-    it("值为undefined", function () {
-        assert.equal(undefined, foo.$val);
+    let foo = new F();
+    foo.on('valueChg', function (value) {
+        assert.equal(value, "saa");
     });
-    it("数据是否修改", function () {
-        assert.equal(false, foo.$isMod);
-    });
+    foo.value = "saa";
 });
 describe("节流效果01", function () {
     let cnt = 0;
@@ -80,25 +120,98 @@ describe("节流效果01", function () {
     });
     let foo = new F();
     it("限制执行01", function () {
-        assert.equal(true, foo.$isValid);
         assert.equal(0, cnt);
     });
     it("限制执行02", function (done) {
-        foo.$val = 5;
+        foo.value = 5;
         // assert.equal(1, cnt);
-        foo.$validate();
+        foo.validate();
         // assert.equal(1, cnt);
-        foo.$validate();
-        foo.$validate().then(
+        foo.validate();
+        foo.validate();
+        foo.validate().then(
             () => {
-                assert.equal(1, cnt);
+                assert.equal(2, cnt);
                 done();
             }
         )
     });
 });
-describe("数组01", function () {
+
+describe("修改状态", function () {
     let F = new Field({
+        name      : "fage",
+        defaultVal: 4,
+        validator : function () {
+            return this.value > 3;
+        }
+    });
+    it("数据是否修改（初始状态）", function () {
+        let foo = new F();
+        assert.equal(false, foo.isModified);
+    });
+    it("数据是否修改（修改）", function () {
+        let foo   = new F();
+        foo.value = '7';
+        assert.equal(true, foo.isModified);
+    });
+});
+
+describe("数组", function () {
+        let F = new Field({
+            name      : "farr",
+            defaultVal: [1, 2, 3, 4],
+            isA       : true,
+            validator : function (val) {
+                return val.length > 3;
+            }
+        });
+        describe("使用默认值", function () {
+            let foo = new F();
+            it("数据是否正确", function () {
+                assert.equal(true, foo.isValid);
+            });
+
+            it("数据是否修改", function () {
+                assert.equal(false, foo.isModified);
+            });
+        });
+        describe("使用初始值", function () {
+            let foo = new F([1, 2]);
+            it("数据是否正确", function () {
+                assert.equal(undefined, foo.isValid);
+            });
+            it("数据是否正确", function (done) {
+                foo.validate().then((isValid) => {
+                    assert.equal(false, foo.isValid);
+                    done();
+                })
+            });
+            it("数据是否修改", function () {
+                assert.equal(false, foo.isModified);
+            });
+        });
+        describe("修改数组后验证有效性", function (done) {
+            let foo = new F([1, 2]);
+            foo.value.push(3, 4);
+            it("数据是否正确", function (done) {
+                foo.validate().then((isValid) => {
+                    assert.equal(true, isValid);
+                    done();
+                })
+            });
+        });
+        describe("修改数组后验证是否修改", function () {
+            let foo = new F([1, 2]);
+            foo.value.push(3, 4);
+            it("数据是否修改", function () {
+                assert.equal(true, foo.isModified);
+            });
+        });
+    }
+);
+describe("异步验证", function () {
+    let F   = new Field({
         name      : "farr",
         defaultVal: [1, 2, 3, 4],
         isA       : true,
@@ -110,97 +223,99 @@ describe("数组01", function () {
             })
         }
     });
-    describe("使用默认值", function () {
-        let foo = new F();
-        it("数据是否正确", function () {
-            assert.equal(true, foo.$isValid);
-        });
+    let foo = new F([1, 2]);
+    it("异步验证", function (done) {
+        foo.validate().then(
+            (ret) => {
+                assert.equal(false, foo.isValid);
+                done();
+            }
+        );
+    });
 
-        it("数据是否修改", function () {
-            assert.equal(false, foo.$isMod);
-        });
-    });
-    describe("使用初始值", function () {
-        let foo = new F([1, 2]);
-        assert.equal(undefined, foo.$isValid);
-        it("数据是否修改", function () {
-            assert.equal(false, foo.$isMod);
-        });
-    });
-    describe("修改值01", function () {
-        let foo = new F([1, 2]);
-        assert.equal(undefined, foo.$isValid);
-        foo.$val.push(3, 4);
-        // assert.equal(true, foo.$isValid);
-        it("数据是否有效", function () {
-            setTimeout(function () {
-                assert.equal(true, foo.$isValid);
-            },300)
-        });
-        it("数据是否修改", function () {
-            assert.equal(true, foo.$isMod);
-        });
-
-    });
-    describe("修改值02", function () {
-        let foo = new F([1, 2]);
-        assert.equal(undefined, foo.$isValid);
-        foo.$val.push(3,5);
-        assert.equal(undefined, foo.$isValid);
-        it("数据是否有效", function () {
-            foo.$validate().then(function () {
-                assert.equal(true, foo.$isValid);
-            });
-        });
-        it("数据是否修改", function () {
-            assert.equal(true, foo.$isMod);
-        });
-
-    });
-    describe("修改值03", function () {
-        let foo = new F([1, 2]);
-        foo.$val=[1,2,3,4];
-        // assert.equal(undefined, foo.$isValid);
-        it("数据是否有效", function () {
-            foo.$validate(function (ret) {
-                assert.equal(true, foo.$isValid);
-            })
-        });
-        it("数据是否修改", function () {
-            setTimeout(function () {
-                assert.equal(true, foo.$isMod);
-            },800)
-
-        });
-    });
-    describe("修改值04", function () {
-        let foo = new F([1, 2]);
-        foo.$val=[1,2,3,4];
-        foo.$validate().then(function () {
-            assert.equal(true, foo.$isValid);
-        });
-        it("数据是否修改", function () {
-            setTimeout(function () {
-                assert.equal(true, foo.$isMod);
-            },800)
-
-        });
-    });
-    describe("修改值05", function () {
-        let foo = new F([1, 2]);
-        // foo.$val=[1,2,3,4];
-        foo.$val.push(3,4);
-        // assert.equal(undefined, foo.$isValid);
-        it("数据是否有效", function () {
-            setTimeout(function () {
-                assert.equal(true, foo.$isValid);
-            },1600);
-
-        });
-        it("数据是否修改", function () {
-            setTimeout(function () {
-                assert.equal(true, foo.$isMod);
-            },800)
-        });
-    });
 });
+
+//     describe("使用初始值", function () {
+//         let foo = new F([1, 2]);
+//         assert.equal(undefined, foo.$isValid);
+//         it("数据是否修改", function () {
+//             assert.equal(false, foo.$isMod);
+//         });
+//     });
+//     describe("修改值01", function () {
+//         let foo = new F([1, 2]);
+//         assert.equal(undefined, foo.$isValid);
+//         foo.$val.push(3, 4);
+//         // assert.equal(true, foo.$isValid);
+//         it("数据是否有效", function () {
+//             setTimeout(function () {
+//                 assert.equal(true, foo.$isValid);
+//             },300)
+//         });
+//         it("数据是否修改", function () {
+//             assert.equal(true, foo.$isMod);
+//         });
+//
+//     });
+//     describe("修改值02", function () {
+//         let foo = new F([1, 2]);
+//         assert.equal(undefined, foo.$isValid);
+//         foo.$val.push(3,5);
+//         assert.equal(undefined, foo.$isValid);
+//         it("数据是否有效", function () {
+//             foo.$validate().then(function () {
+//                 assert.equal(true, foo.$isValid);
+//             });
+//         });
+//         it("数据是否修改", function () {
+//             assert.equal(true, foo.$isMod);
+//         });
+//
+//     });
+//     describe("修改值03", function () {
+//         let foo = new F([1, 2]);
+//         foo.$val=[1,2,3,4];
+//         // assert.equal(undefined, foo.$isValid);
+//         it("数据是否有效", function () {
+//             foo.$validate(function (ret) {
+//                 assert.equal(true, foo.$isValid);
+//             })
+//         });
+//         it("数据是否修改", function () {
+//             setTimeout(function () {
+//                 assert.equal(true, foo.$isMod);
+//             },800)
+//
+//         });
+//     });
+//     describe("修改值04", function () {
+//         let foo = new F([1, 2]);
+//         foo.$val=[1,2,3,4];
+//         foo.$validate().then(function () {
+//             assert.equal(true, foo.$isValid);
+//         });
+//         it("数据是否修改", function () {
+//             setTimeout(function () {
+//                 assert.equal(true, foo.$isMod);
+//             },800)
+//
+//         });
+//     });
+//     describe("修改值05", function () {
+//         let foo = new F([1, 2]);
+//         // foo.$val=[1,2,3,4];
+//         foo.$val.push(3,4);
+//         // assert.equal(undefined, foo.$isValid);
+//         it("数据是否有效", function () {
+//             setTimeout(function () {
+//                 assert.equal(true, foo.$isValid);
+//             },1600);
+//
+//         });
+//         it("数据是否修改", function () {
+//             setTimeout(function () {
+//                 assert.equal(true, foo.$isMod);
+//             },800)
+//         });
+//     });
+// });
