3ed0768384144b55eebf4039a44ece9672ffb4cc
chunge <chunge@localhost>
2021-12-27 20:30:58 +0800

    fix: some error

--- src/field.js
+++ src/field.js
@@ -1,102 +1,67 @@
-let Evt      = require("../assets/evt");
-let debounce = require("../assets/throttle").debounce;
+let isDiff = require("./is-diff");
+
+let throttle = require("../assets/throttle").throttle;
+let Evt           = require("../assets/evt");
+
+function proxyArray(initVal = [], fn) {
+    let arr              = [...initVal];
+    let myArrayPrototype = Object.create(Array.prototype);
+    [
+        "push", "pop", "shift", "unshift", "splice", "sort", "reverse"
+    ].forEach(method => {
+        let m                    = Array.prototype[method];
+        myArrayPrototype[method] = function () {
+            let pre    = [...this];
+            let result = m.apply(this, arguments);
+            fn(this, pre);
+            return result;
+        };
+    });
+    arr.__proto__ = myArrayPrototype;
+    return arr;
+}
+
+function ifDirtyFn(newValue, initValue, preValue) {
+    let isModified = isDiff(newValue, initValue);
+    this.$ref && this.$ref.$emit("fieldValueChg", this.name, newValue, preValue);
+    this.$validate();
+    if (this.isModified !== isModified) {
+        this.isModified = isModified;
+        this.$ref && this.$ref.$emit("fieldModChg", this.name, isModified);
+    }
+}
+
 
 /**
  *
  * @param initVal
+ * @param ctx
  * @constructor
  */
-function Destor(initVal) {
-    initVal===undefined&&(initVal="");
+
+function Destor(initVal, ctx) {
+    // initVal === undefined && (initVal = "");
     let value = initVal;
-    this.get  = () => value;
-    this.set  = function (newValue) {
-        newValue===undefined&&(newValue="");
-        let isMod = Array.isArray(newValue) ? wrapArr.isDiff(newValue, value) : value !== newValue;
-        if (!isMod) return value;
-        this.emit("valueChg", newValue, value, this);
-        this.ref && this.ref.emit("fieldValueChg", this.name, newValue, value);
-        if (!Array.isArray(newValue) && this.isModified !== (newValue !== initVal)) {
-            this.isModified = !this.isModified;
-            this.emit("modChg", this.isModified);
-            this.ref && this.ref.emit("fieldModChg", this.name, this.isModified);
-        }
-        value = newValue;
-        Array.isArray(value) || this.validate();//数组另处理
+    let fn    = (newVal, preVal) => ifDirtyFn.call(ctx, newVal, initVal, preVal)
+    if (initVal && Array.isArray(initVal)) {
+        value = proxyArray(initVal, fn);
+    }
+    this.get = () => value;
+    this.set = function (newValue) {
+        if (!isDiff(newValue, value)) return value;
+        Array.isArray(newValue) && (newValue = proxyArray(newValue, fn));
+        value = newValue
+        fn(newValue, value);
         return value;
-        // if(this.$isValid===undefined)this.$validate();
     };
 }
-
-
-let wrapArr = (function () {
-    const arrayProto = Array.prototype;
-    const wrapProto  = Object.create(arrayProto);
-    [
-        "push",
-        "pop",
-        "shift",
-        "unshift",
-        "splice",
-        "sort",
-        "reverse"
-    ].forEach(method => {
-        let m             = arrayProto[method];
-        wrapProto[method] = function mutator() {
-            let _arr_ = [...this];
-            let $ctx  = this.$ctx;
-            m.apply(this, arguments);
-            // this.$ctx.$ref[this.$ctx.$name]=this;
-            $ctx.emit("valueChg", this, _arr_, $ctx);
-            $ctx.ref && $ctx.ref.emit("fieldValueChg", $ctx.name, this);
-        };
-    });
-    let _valChgHandler_ = function (initVal, val, ctx) {
-        // let ctx=nval.$ctx;
-        ctx.validate(val);
-        let isMod = _wrapArr_.isDiff(initVal, val);
-        if (isMod === ctx.isModified) return;
-        ctx.isModified = isMod;
-        ctx.emit("modChg", isMod);
-        ctx.ref && ctx.ref.emit("fieldModChg", ctx.name, isMod);
-    };
-
-    // _valChgHandler_.promiseble(false);
-    function _wrapArr_(initVal = [], ctx) {
-        let _val_       = [...initVal];
-        _val_.__proto__ = wrapProto;
-        Object.defineProperty(_val_, "$ctx", {value: ctx});
-        ctx.on("valueChg", (newVal, preVal, ctx) => _valChgHandler_(initVal, newVal, ctx));
-        return _val_;
-    }
-
-    _wrapArr_.isDiff = function (a1, a2) {
-        // if (a1 === a2) return false;
-        if ((a1 && !a2) || (!a1 || a2) || a1.length !== a2.length) return true;
-        for (let i = 0, n = a1.length; i < n; i++)
-            if (!a2.includes(a1[i])) return true;
-        return false;
-    };
-    return _wrapArr_;
-})();
-
 
 /**
- * generate a Field Type
- * @param conf 字段属性配置
- * @return {F}
- * @constructor
+ *
+ * @param isValid
+ * @returns {*}
+ * @private
  */
-function __(isValid) {
-    let flag     = this.isValid !== isValid;
-    this.isValid = isValid;
-    if (flag) {
-        this.emit("validChg", isValid);
-        this.ref && this.ref.emit("fieldValidChg", this.name, isValid);
-    }
-    return isValid;
-}
-
 function FieldPrototype({name, alias, desc, validator, defaultValue, required}) {
     this.name         = name;
     this.alias        = alias;
@@ -111,58 +76,49 @@ function FieldPrototype({name, alias, desc, validator, defaultValue, required}) 
  *
  */
 FieldPrototype.prototype.isField = true;
-
-FieldPrototype.prototype.validate = function () {
-    let val       = this.value;
-    let isValid   = undefined;
-    let validator = this.validator;
-    if (val === this.defaultValue || (Array.isArray(val) && !wrapArr.isDiff(val, this.defaultValue))) {
+const validateHelper               = function (isValid) {
+    if (this.isValid !== isValid) {
+        this.isValid = isValid;
+        this.$ref && this.$ref.$emit("fieldValidChg", this.name, isValid);
+    }
+    this.$validate.isValid = isValid;
+    return isValid;
+}
+FieldPrototype.prototype.$validate = function () {
+    let isValid, val = this.value;
+    let validator    = this.validator;
+    if (val === this.defaultValue || (Array.isArray(val) && !isDiff(val, this.defaultValue))) {
         isValid = true;
     } else if (val === undefined || val === "") {
         isValid = !this.required;
     } else {
         isValid = typeof validator === "function" ? this.validator(val) : validator && this.validator.test(val);
     }
-    isValid = isValid && isValid.then ? isValid : Promise.resolve(isValid);
+    isValid = typeof isValid === "object" && isValid && isValid.then ? isValid : Promise.resolve(isValid);
+    if (isValid && isValid.then) {
+        isValid.then(i => Reflect.apply(validateHelper, this, [i]))
+    } else {
+        Reflect.apply(validateHelper, this, [isValid])
+    }
     return isValid;
 };
-FieldPrototype.prototype.on       = function () {
-    this.evt || (this.evt = new Evt());
-    this.evt.subscribe(...arguments);
-};
-
-FieldPrototype.prototype.emit = function () {
-    this.evt && this.evt.trigger(...arguments);
-};
-module.exports                = function defineField(conf) {
-    function F(value, isValid) {
+module.exports                     = function defineField(conf) {
+    function F(value, isValid, ref) {
         if (value === undefined) {
-            this.required?value = this.defaultValue:isValid=true;
+            this.required ? value = this.defaultValue : isValid = true;
         }
         if (isValid === undefined) {
-            //值不能为空
             if (this.required && value === undefined) {
                 isValid = false;
             } else {
-                if ((value === undefined && !this.required) || (this.defaultValue === value)) isValid = true;
+                isValid = !!((value === undefined && !this.required) || (this.defaultValue === value));
             }
         }
-
-        Object.defineProperty(this, "value", new Destor(Array.isArray(value) ? wrapArr(value, this) : value));
+        Object.defineProperty(this, "value", new Destor(value, this));
         this.isValid    = isValid;
         this.isModified = false;
-        let validateFn  = debounce(this.validate, 100, {immediate: true, promise: true});
-        let validCache;
-        this.validate   = function (flag) {
-            //flag表示可以利用上一次的验证结果；
-            if (flag === true&&validCache) return validCache.then?validCache:Promise.resolve(validCache);
-            let valid  = validateFn.call(this, this.value);
-            validCache = valid.then((val) => {
-                validCache = val;
-                return __.call(this, val);
-            });
-            return validCache;
-        }
+        this.$ref       = ref;
+        this.$validate  = throttle(this.$validate, 200, {immediate: false, promise: true});
     }
 
     F.prototype             = new FieldPrototype(conf);
--- src/model.js
+++ src/model.js
@@ -3,141 +3,107 @@ let Evt           = require("../assets/evt");
 let debounce      = require("../assets/throttle").debounce;
 let {has, notAll} = require("../assets/object");
 
+function ckValidationHelper(isValid, validation, ctx) {
+    isValid && (isValid = !notAll(validation, true));
+    if (isValid !== ctx.$isValid) {
+        ctx.ref && (ctx.ref.$validation[ctx.name] = isValid);
+        ctx.$emit("validChg", isValid);
+        ctx.ref && ctx.ref.$emit('fieldValidChg', ctx.name, isValid);
+    }
+    return ctx.$isValid = isValid;
+}
+
 /**
  *
  * @param fieldsCfg
  * @constructor
  */
 function ModelPrototype(fieldsCfg) {
-    Object.defineProperty(this, "fields", {
-        value   : [],
-        writable: true
-    });
     let fields = [];
     for (let field of fieldsCfg) {
-        if (!field.isField && !field.isModel) {
-            field = field.fields ? defineModel(field) : defineField(field)
+        if (!field.isField && !field.$isModel) {
+            field = field.$isModel ? defineModel(field) : defineField(field)
         }
         fields.push(field);
     }
-    this.fields = fields;
+    this.$fields = fields;
     return this;
 }
 
+
 /**
  *
  */
-ModelPrototype.prototype.emit = function () {
-    this.evt && this.evt.trigger(...arguments);
-};
-/**
- *
- */
-ModelPrototype.prototype.on = function () {
-    this.evt || (Object.defineProperty(this, "evt", {value: new Evt()}));
-    this.evt.subscribe(...arguments);
-};
-
-
+// ModelPrototype.prototype.$emit = function () {
+//    this.$evt.trigger(...arguments);
+// };
+// /**
+//  *
+//  */
+// ModelPrototype.prototype.$on = function () {
+//     this.$evt.subscribe(...arguments);
+// };
+// /**
+//  *
+//  */
+// ModelPrototype.prototype.$watch = function (name, fn) {
+//     this.$evt.subscribe(...arguments);
+// };
 /**
  *
  * @param validateAll undefined,false,true
  * @return {Promise<any[]>}
  */
-ModelPrototype.prototype.validate = function (validateAll) {
+ModelPrototype.prototype.$validate = function (validateAll) {
     let ths       = this,
-        fieldData = ths.fieldData,
+        fieldData = ths.$fields,
         varr      = [],
         fields    = Object.keys(fieldData);
     for (let fieldName of fields) {
         let field = fieldData[fieldName];
-        // if (field.isField && field.isValid !== undefined && !field.validate.ing) continue;
-        // tmp            = field.isValid;
-        let tmp = field.validate(true);
+        let tmp   = field.$validate();
         varr.push(tmp);
     }
-    return Promise.all(varr).then((ret) => {
-        let isValid = !notAll(ths.validation, true);
-        if (isValid !== ths.isValid) {
-            ths.ref && (ths.ref.validation[ths.name] = isValid);
-            ths.emit("validChg", isValid);
-            ths.ref && ths.ref.emit('fieldValidChg', ths.name, isValid);
-        }
-        ths.isValid = isValid;
-        return ths.isValid;
-    });
+    return Promise.all(varr).then((ret) => ckValidationHelper(true, ths.$validation, this));
 };
 
-ModelPrototype.prototype.isModel = true;
-
-
-/**
- *
- * @param arg
- */
-function getFieldAlias(arg) {
-    let ret    = {}, field,
-        fields = this.prototype.fields;
-    if (arg) {
-        for (let field of fields)
-            if (field.name === arg)
-                return field.prototype.isField ? field.prototype.alias : field.getAlias()
-    } else {
-        for (let field of fields)
-            ret[field.name] = field.prototype.isField ? field.prototype.alias : field.getAlias();
-    }
-    return ret;
-}
+ModelPrototype.prototype.$isModel = true;
 
 function fieldValidChgHandler(ctx) {
     let _ = debounce(
         function (isValid, validation) {
-            isValid && (isValid = !notAll(validation, true));
-            if (isValid === ctx.isValid) return;
-            ctx.isValid = isValid;
-            ctx.emit("validChg", isValid);
-            ctx.ref && ctx.ref.emit("fieldValidChg", ctx.constructor.name, isValid);
-            return isValid;
+            return ckValidationHelper(isValid, validation, ctx);
         }, 80, {promise: true}
     );
     return function (fieldName, isValid) {
-        ctx.validation[fieldName] = isValid;
-        if (isValid === ctx.isValid) return;
+        ctx.$validation[fieldName] = isValid;
+        if (isValid === ctx.$isValid) return;
         if (isValid === true) {
-            _(isValid, ctx.validation)
+            _(isValid, ctx.$validation)
         } else {
-            ctx.isValid = false;
-            ctx.ref && ctx.ref.emit("fieldValidChg", ctx.constructor.name, false);
-            ctx.emit("validChg", isValid);
+            ctx.$isValid = false;
+            ctx.$ref && ctx.$ref.$emit("fieldValidChg", ctx.constructor.name, false);
+            ctx.$emit("validChg", isValid);
         }
     }
 }
 
 function fieldModChgHandler(ctx) {
     let _ = debounce(
-        function (modified) {
-            let isMod = has(modified, true);
-            if (isMod === ctx.isModified) return;
-            ctx.isModified = isMod;
-            ctx.ref && ctx.ref.emit("fieldModChg", ctx.name, ctx.isModified);
-            // if (ctx.ref) {
-            // ctx.ref.modified[ctx.name] = true;
-            // }
-            ctx.emit("modChg", ctx.isModified);
+        function (modified, isMod) {
+            isMod = isMod || has(modified, true);
+            if (isMod === ctx.$isModified) return;
+            ctx.$isModified = isMod;
+            ctx.$ref && ctx.$ref.$emit("fieldModChg", ctx.name, ctx.$isModified);
+            ctx.$emit("modChg", ctx.$isModified);
+            ctx.$emit('$isModified', ctx.$isModified);
             return isMod;
-        }, 80, {promise: true}
+        }, 80, {promise: true, immediate: true}
     );
     return function (fieldName, isMod) {
-        ctx.modified[fieldName] = isMod;
-        if (isMod === ctx.isModified) return;
-        if (isMod === true) {
-            ctx.isModified = true;
-            ctx.emit("modChg", true);
-            ctx.ref && ctx.ref.emit("fieldModChg", ctx.name, ctx.isModified);
-        } else {
-            _(ctx.modified)
-        }
-        // if (isMod !== ctx.isModified) isMod ? ctx.isModified = true : _(ctx.modified);
+        ctx.$modified[fieldName] = isMod;
+        if (isMod === ctx.$isModified) return;
+        _(ctx.$modified, isMod);
     }
 }
 
@@ -156,70 +122,56 @@ function defineModel(cfg) {
      * @private
      */
     let _       = function (data, isValid) {
-        let flag         = data ? Array.isArray(data) : data = {},
-            modified     = {},
-            dataObj      = {},
-            fieldData    = {},
-            valueChgFlag = {},
-            validateFlag = 0,
-            fields       = this.fields;
-        this.validation  = {};
-        this.isModified  = false;
+        let flag          = data ? Array.isArray(data) : data = {},
+            modified      = {},
+            validateFlag  = 0,
+            fields        = this.$fields;
+        this.$validation  = {};
+        this.$isModified  = false;
+        this.$fields      = {};
+        const evt         = new Evt();
+        this.$onEvent     = (name, fn) => evt.subscribe(name, fn);
+        this.$removeEvent = (name, fn) => evt.remove(name, fn);
+        this.$emit        = function () {
+            evt.trigger(...arguments);
+        };
         for (let idx = 0, len = fields.length; idx < len; idx++) {
             let field,
-                fieldCls           = fields[idx],
-                fname              = fieldCls.name,
-                initVal            = flag ? data[idx] : data[fname];
-            field                  = new fieldCls(initVal, isValid);
-            field.ref              = this;
-            field.idx              = idx;
-            modified[fname]        = false;
-            fieldData[fname]       = field;
-            this.validation[fname] = field.isModel ? field.validation : field.isValid;
+                fieldCls            = fields[idx],
+                fname               = fieldCls.name,
+                initVal             = flag ? data[idx] : data[fname];
+            field                   = new fieldCls(initVal, isValid, this);
+            field.idx               = idx;
+            modified[fname]         = false;
+            this.$fields[fname]     = field;
+            this.$validation[fname] = field.$isModel ? field.$validation : field.isValid;
             if (isValid === undefined && !field.isValid) validateFlag = validateFlag | (field.isValid === false ? 1 : 2);
-            Object.defineProperty(dataObj, fname, {
+            Object.defineProperty(this, fname, {
                 set         : function (value) {
-                    if (valueChgFlag[fname]) {
-                        return delete valueChgFlag[fname]
-                    }
-                    field.value = value;
+                    field.$isModel ? Object.assign(field, value) : field.value = value;
                 },
                 get         : () => field.value,
                 enumerable  : true,
                 configurable: true
             });
         }
-        this.fieldData = fieldData;
         if (isValid === undefined) {
-            this.isValid = validateFlag ? (validateFlag & 1 ? false : undefined) : true;
+            this.$isValid = validateFlag ? (validateFlag & 1 ? false : undefined) : true;
         } else {
-            this.isValid = isValid;
+            this.$isValid = isValid;
         }
-        Object.defineProperty(this, "modified", {value: modified});
-        Object.defineProperty(this, "value", {
-            get         : () => dataObj,
-            set         : function (data) {
-                for (let name in data) {
-                    if (!data.hasOwnProperty(name)) continue;
-                    let field = this.fieldData[name];
-                    field && (field.value = data[name]);
-                }
-            },
-            enumerable  : true,
-            configurable: true
+        this.$modified = modified;
+        this.$onEvent("fieldValidChg", fieldValidChgHandler(this));
+        this.$onEvent("fieldModChg", fieldModChgHandler(this));
+        this.$onEvent("fieldValueChg", (fname, value) => {
+            evt.trigger(fname, value);
         });
-        this.on("fieldValidChg", fieldValidChgHandler(this));
-        this.on("fieldModChg", fieldModChgHandler(this));
-        this.on("fieldValueChg", (fname, value) => {
-            valueChgFlag[fname] = true;
-            dataObj[fname]      = value;
-        });
-        this.validate = debounce(this.validate, 100, {immediate: true, promise: true});
+        this.$validate = debounce(this.$validate, 100, {immediate: true, promise: true});
     };
     _.prototype = new ModelPrototype(fields);
+    _.$fields   = _.prototype.$fields;
     cfg.name && Object.defineProperty(_, "name", {value: cfg.name});
     _.prototype.constructor = _;
-    _.getAlias              = getFieldAlias;
     return _;
 }
 
