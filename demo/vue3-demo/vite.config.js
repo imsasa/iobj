b06bdb5d82102391811bb912c3ac15675b37cd99
chunge <>
2026-01-05 16:12:40 +0800

    feature: - 添加vue示例页面

--- demo/vue3-demo/vite.config.js
+++ demo/vue3-demo/vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   root: 'src',
+  build: {
+    outDir: '../dist'
+  },
   resolve: {
     dedupe: ['vue']
   },
